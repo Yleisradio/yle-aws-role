f48f5ae5d86b163f40372a5b914004016c6a43fb
Teemu Matilainen <teemu.matilainen@iki.fi>
2017-05-16 13:29:36 +0300

    Bump version for development

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.0.2 / _Not released yet_
+
+
 ## 1.0.1 / 2017-05-16
 
 - Initial public release
--- lib/yle/aws/role/version.rb
+++ lib/yle/aws/role/version.rb
@@ -1,7 +1,7 @@
 module Yle
   module AWS
     class Role
-      VERSION = '1.0.1'.freeze
+      VERSION = '1.0.2.dev'.freeze
     end
   end
 end
